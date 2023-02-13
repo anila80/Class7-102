51a959ac0b5aa66efcf6c729ff0be3af61414f01
Anila <anilarahimkhan@hotmail.com>
2023-02-13 17:18:28 -0600

    new

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function userName() {
+
+    let thisName;
+    while (thisName != "5") {
+        thisName = prompt("What is 2 +  3?");
+        if (thisName == "5") {
+            alert("You are right")
+        }
+        else {
+            alert("Try Again")
+        }
+    }
+}
